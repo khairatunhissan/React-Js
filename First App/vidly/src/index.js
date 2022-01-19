e305fbc60386434b013220b37ec5527b28025b1f
khairatunhissan <khairatunhissan@gmail.com>
2022-01-19 15:55:02 +0600

    Component

--- First App/vidly/src/index.js
+++ First App/vidly/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
